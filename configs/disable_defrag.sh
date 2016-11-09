29d5d4af6bd411913848483f9842ac83124938fe
David Twersky <davidtw@bhphoto.com>
2016-11-09 11:47:54 -0500

    added to echo never to thp enabled

--- configs/disable_defrag.sh
+++ configs/disable_defrag.sh
@@ -3,3 +3,7 @@
 if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
   echo never > /sys/kernel/mm/transparent_hugepage/defrag
 fi
+
+if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+  echo never > /sys/kernel/mm/transparent_hugepage/enabled
+fi
